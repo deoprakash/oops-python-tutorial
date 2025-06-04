f326ee1ced2d041583bb1f397cafd16da5b74503
DEO PRAKASH <deo.prakash@ssipmt.com>
2025-06-05 00:11:10 +0530

    class and object basic done

---
+++ oops1.py
@@ -0,0 +1,23 @@
+class employee:  #class initiated
+
+    #special method/logic
+    def __init__(self):
+        print("Started executing data")
+        self.id= 123
+        self.salary = 50000
+        self.designation = "SDE"
+        print("data has been initiated")
+
+    # user defined methods
+    def travel(self, destination):
+        print("Travel method was called manually")
+        print(f"Employee is travelling to {destination}")
+
+#create an obj/instance of class
+sam = employee()
+
+# printing attributes
+# print(sam.id)
+
+# calling method
+# sam.travel('Mumbai')
